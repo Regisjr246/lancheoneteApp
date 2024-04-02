391b34ce329749feadc1827c312d07c78aeb15ef
Regisjr246 <regisjr246@gmail.com>
2024-04-02 15:35:34 -0300

    git

--- src/screens/CadastroProduto.tsx
+++ src/screens/CadastroProduto.tsx
@@ -1,5 +1,6 @@
 import React, { useState } from 'react';
 import { Image, StatusBar, StyleSheet, Text, TextInput, TouchableOpacity, View } from "react-native";
+import { launchCamera } from 'react-native-image-picker';
 
 const CadastroProduto: React.FC = () => {
     const [produtos, setProdutos] = useState<Produto[]>([]);
@@ -12,6 +13,31 @@ const CadastroProduto: React.FC = () => {
     const CadastroProduto = async () => {
 
     }
+
+
+    const abrirCamera = () => {
+        const options = {
+            mediaType: 'photo',
+            includeBase64: false,
+            maxHeight: 2000,
+            macWidth: 2000
+        };
+        launchCamera(options, response => {
+            if (response.didCancel) {
+                console.log('cancelad pelo usuário');
+            } else if (response.error) {
+                console.log("erro ao abrir a camera");
+            } else {
+                let imageUri = response.uri || response.assets?.[0]?.uri;
+                setImagem(imageUri);
+                console.log(imageUri);
+            }
+        });
+    }
+
+
+
+
     return (
         <View style={styles.container}>
             <StatusBar backgroundColor="red" barStyle="light-content" />
@@ -20,29 +46,29 @@ const CadastroProduto: React.FC = () => {
             </View>
             <View style={styles.form}>
                 <TextInput style={styles.input} placeholder="Nome do Produtos" value={nome} onChangeText={setNome} />
-                <TextInput style={styles.input} placeholder="Precço" value={preco} onChangeText={setPreco} />
+                <TextInput style={styles.input} placeholder="Preço" value={preco} onChangeText={setPreco} />
                 <TextInput style={styles.input} placeholder="Ingredientes" value={ingredientes} onChangeText={setIngredientes} multiline />
                 <View style={styles.alinhamnetoImagemSelecionado}>
-                    {imagem ? <Image source={{ uri: imagem }} style={styles.imagemSelecionando} />:null}
+                    {imagem ? <Image source={{ uri: imagem }} style={styles.imagemSelecionando} /> : null}
                 </View>
 
 
 
-<TouchableOpacity style={styles.imageButton}>
-    <Text style={styles.imageButtonText}> Selecionar Imagem</Text>
-       </TouchableOpacity>
+                <TouchableOpacity style={styles.imageButton}>
+                    <Text style={styles.imageButtonText}> Selecionar Imagem</Text>
+                </TouchableOpacity>
 
 
-       <TouchableOpacity style={styles.imageButton}>
-    <Text style={styles.imageButtonText}>Tirar Foto</Text>
-       </TouchableOpacity>
+                <TouchableOpacity style={styles.imageButton} onPress={abrirCamera}>
+                    <Text style={styles.imageButtonText}>Tirar Foto</Text>
+                </TouchableOpacity>
 
-       
-<TouchableOpacity style={styles.imageButton}>
-    <Text style={styles.imageButtonText}> Cadastar Produto</Text>
-       </TouchableOpacity>
 
-    
+                <TouchableOpacity style={styles.imageButton}>
+                    <Text style={styles.imageButtonText}> Cadastar Produto</Text>
+                </TouchableOpacity>
+
+
 
 
 
